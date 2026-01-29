f90370c19296ef0bc80410aa8f703759c222d62c
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-01-29 23:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (86.71%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        count = 0
+        for h in hours:
+            if h >= target:
+                count += 1
+        return count

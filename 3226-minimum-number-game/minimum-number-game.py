8970f64cd91506721dad9b9d7ec28d8bfcf00821
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-02-04 11:24:40 +0530

    Time: 7 ms (29.58%) | Memory: 12.6 MB (0.47%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numberGame(self, nums):
+        nums.sort()
+        arr = []
+        for i in range(0, len(nums), 2):
+            arr.append(nums[i + 1])
+            arr.append(nums[i])
+        return arr

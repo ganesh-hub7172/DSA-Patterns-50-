8541003713ad9653aa68941c1d3e0e52b6f1c496
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-02-04 11:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (18.60%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def restoreString(self, s, indices):
+        res = [''] * len(s)
+        
+        for i, ch in enumerate(s):
+            res[indices[i]] = ch
+        
+        return ''.join(res)

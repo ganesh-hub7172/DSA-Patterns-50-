59ef3d9dc3e7a16ad82feef53e7580606c8f6eab
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-02-20 10:37:11 +0530

    Time: 2 ms (41.18%) | Memory: 12.5 MB (64.71%) - LeetSync

---
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def distributeCoins(self, root):
+        self.moves = 0
+        
+        def dfs(node):
+            if not node:
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            
+            self.moves += abs(left) + abs(right)
+            
+            return node.val + left + right - 1
+        
+        dfs(root)
+        return self.moves

5f106fa9fa38972f2ce39fad619d159bc95ea531
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-03-03 09:45:04 +0530

    Time: 11 ms (73.00%) | Memory: 21.1 MB (43.66%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -1,17 +1,20 @@
 class BSTIterator:
+
     def __init__(self, root):
         self.stack = []
-        self._push_left(root)
+        self._pushLeft(root)
 
-    def _push_left(self, node):
+    def _pushLeft(self, node):
         while node:
             self.stack.append(node)
             node = node.left
 
     def next(self):
         node = self.stack.pop()
+        
         if node.right:
-            self._push_left(node.right)
+            self._pushLeft(node.right)
+        
         return node.val
 
     def hasNext(self):

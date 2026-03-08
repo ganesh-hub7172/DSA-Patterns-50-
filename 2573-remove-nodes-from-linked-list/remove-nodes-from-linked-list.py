67f0b11eaf8a7625388ed803fc8a2237b190ee6f
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-03-08 07:47:57 +0530

    Time: 219 ms (74.77%) | Memory: 91.9 MB (99.08%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -0,0 +1,26 @@
+class Solution:
+    def removeNodes(self, head):
+        
+        def reverse(head):
+            prev = None
+            curr = head
+            while curr:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+            return prev
+        
+        head = reverse(head)
+        
+        max_val = head.val
+        curr = head
+        
+        while curr and curr.next:
+            if curr.next.val < max_val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+                max_val = curr.val
+        
+        return reverse(head)
